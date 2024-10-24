d175ee4995db45d735d2bcb3059efd30916b3778
Marina Mansueto <mansumdp@gmail.com>
2024-10-24 17:50:05 -0300

    override, body parser y configuracion ejs

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const methodOverride = require('method-override');
 
 const adminRoutes = require('./src/routes/adminRoutes.js');
 const authRoutes = require('./src/routes/authRoutes.js');
@@ -8,7 +10,16 @@ const shopRoutes = require('./src/routes/shopRoutes.js');
 
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname ,'./src/views'));
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.set
+app.use(methodOverride('_method'))
+
 app.use(express.static('public'));
+
 app.use('/', mainRoutes);
 app.use('/shop', shopRoutes);
 app.use('/admin', adminRoutes);
