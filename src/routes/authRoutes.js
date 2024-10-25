b063e52f5290bf1968ad836dc68f09194a277908
Marina Mansueto <mansumdp@gmail.com>
2024-10-25 08:23:02 -0300

    authRoutes texto plano

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const authControllers = require('../controllers/authControllers');
 
-
-router.get('/login', (req, res) => res.send("vista de login"));
-router.post('/login', (req, res) => res.send("ruta que válida datos del login"));
-router.get('/register', (req, res) => res.send("vista de register"));
-router.post('/register', (req, res) => res.send("ruta que crea nuevo usuario"));
-router.get('/logout', (req, res) => res.send("ruta que cierra sesión"));
+router.get('/login', authControllers.login );
+router.post('/login', authControllers.doLogin);
+router.get('/register',authControllers.register);
+router.post('/register', authControllers.doRegister);
+router.get('/logout', authControllers.logout);
 
 
 
