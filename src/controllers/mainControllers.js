1bc67b20588c35ceb2bf7a51bb138f4eb8193412
Marina Mansueto <mansumdp@gmail.com>
2024-10-25 09:04:08 -0300

    correccion ruta index

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,10 +1,5 @@
 module.exports = {
-   /*si quiero que el index sea "Home"
-    home: (req, res) => {
-        res.render('index');
-    },
-    */
-    home: (req, res) => res.send("Página de Home"),
+    home: (req, res) => res.render('index'),
     contact: (req, res) => res.send("Página de Contacto"),
     about: (req, res) => res.send("Página Sobre Nosotros")
    }
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainControllers.js');
 
-/*router.get('/', mainController.home);*/
+router.get('/', mainController.home);
 router.get('/home', mainController.home);
 router.get('/contact', mainController.contact);
 router.get('/about', mainController.about);
