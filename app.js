b501a7c51908fc6d3db3289376b2cd30e3df0931
Marina Mansueto <mansumdp@gmail.com>
2024-10-25 08:03:48 -0300

    express.urlencoded extended

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname ,'./src/views'));
-app.use(express.urlencoded());
+/*app.use(express.urlencoded());*/
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 app.set
