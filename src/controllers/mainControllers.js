86c446310a7caed86c4c2f8a6eadfbc0848315fc
Marina Mansueto <mansumdp@gmail.com>
2024-10-31 08:24:45 -0300

    header y footer ejs

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,5 +1,13 @@
 module.exports = {
-    home: (req, res) => res.render('index'),
-    contact: (req, res) => res.render('contact'),
-    about: (req, res) => res.render('about')
+    home: (req, res) => 
+        res.render('index', {
+            title: 'Inicio'
+        }),
+    contact: (req, res) => 
+        res.render('contact', {
+            title: 'Contacto'
+        }),
+    about: (req, res) => res.render('about', {
+        title: 'Sobre nosotros'
+    })
 }
