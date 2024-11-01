ac0bfa5c3054fcadf9ebc66c663a9c2412b825fc
Marina Mansueto <mansumdp@gmail.com>
2024-11-01 06:55:55 -0300

    archivo de conexión a BD

--- src/config/conn.js
+++ src/config/conn.js
@@ -9,7 +9,14 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit:0
 });
-
+pool.getConnection((error, connection) => { //1° parámetro error, 2°parámetro conexión realizada
+    if (error) {
+        console.error('error al obtener la conexión a la base de datos', error);
+        } else {
+            console.log('conexión a la base de datos exitosa');
+            connection.release(); //libero conexión
+        }
+});
 
 module.exports = {
     conn: pool.promise()
